4bf5f70a9562b4447a0d7a8f2a740f50b2675852
Oluwaferanmi Heka <adedokunoluwaferanmi@gmail.com>
2020-10-07 19:51:24 +0100

    +fixes

--- menu.js
+++
@@ -1,8 +0,0 @@
-let button = document.querySelector('.hamburger');
-let navigation = document.querySelector('.navigation-section');
-function toggle() {
-    this.classList.toggle('clicked');
-    navigation.classList.toggle('clicked');
-};
-
-button.addEventListener('click', toggle);
